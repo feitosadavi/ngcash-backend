557fa195471d1f06ceb4b9eb6441dd41c3210235
feitosadavi <davifeitosa.dev@protonmail.com>
2022-11-15 02:32:43 -0300

    feat(test)!: ensure it calls createUserRepository with correct input

--- tests/modules/user/services/CreateUser.service.spec.ts
+++ tests/modules/user/services/CreateUser.service.spec.ts
@@ -4,6 +4,7 @@ import { CreateUserService } from '../../../../src/modules/user/services'
 // import { CreateUserService } from '@/'
 
 import { IHasherAdapter } from 'shared/data/adapters/cryptography'
+import { ICreateUserRepository } from 'modules/user/services/repository.protocols'
 
 const makeFakeCreateUserModel = (): ICreateUserModel => ({
 	username: 'any_username',
@@ -13,6 +14,7 @@ const makeFakeCreateUserModel = (): ICreateUserModel => ({
 describe('CreateUserService', () => {
 	let sut: CreateUserService
 	let fakeHasher: MockProxy<IHasherAdapter>
+	let fakeCreateUserRepository: MockProxy<ICreateUserRepository>
 
 	let fakeCreateUserModel: ICreateUserModel
 
@@ -20,15 +22,24 @@ describe('CreateUserService', () => {
 		fakeHasher = mock()
 		fakeHasher.hash.mockResolvedValue('hashed_password')
 
+		fakeCreateUserRepository = mock()
+		fakeCreateUserRepository.create.mockResolvedValue(true)
+
 		fakeCreateUserModel = makeFakeCreateUserModel()
 	})
 
 	beforeEach(() => {
-		sut = new CreateUserService(fakeHasher)
+		sut = new CreateUserService(fakeHasher, fakeCreateUserRepository)
 	})
 
 	it('should call hasher with correct input', async () => {
 		await sut.execute(fakeCreateUserModel)
 		expect(fakeHasher.hash).toHaveBeenCalledWith('any_password')
+	})
+
+	it('should call createUserRepository with correct input', async () => {
+		await sut.execute(fakeCreateUserModel)
+		expect(fakeCreateUserRepository.create)
+			.toHaveBeenCalledWith({ ...fakeCreateUserModel, password: 'hashed_password' })
 	})
 })
